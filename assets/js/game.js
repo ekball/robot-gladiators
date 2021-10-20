6fbac9aa1178bac580339be8d9f38fa5ee4fe1eb
ekball <90882418+ekball@users.noreply.github.com>
2021-10-20 15:08:50 -0400

    fexed the skip/fight bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,24 +1,49 @@
+var fightOrSkip = function() {
+
+    // ask player if they'd like to fight or skip using fightOrSkip function
+    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+    // Conditional Recursive Function Call
+    if (promptFight === "" || promptFight === null) {
+
+        window.alert("You need to provide a valid answer! Please try again.");
+
+        return fightOrSkip();
+    }
+
+
+  // if player picks "skip" confirm and then stop the loop
+  promptFight = promptFight.toLowerCase();
+
+  if (promptFight === "skip") {
+
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    if (confirmSkip) {
+
+      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+
+      // subtract money from playerMoney for skipping, but don't let them go into the negative
+      playerInfo.money = Math.max(0, playerInfo.money - 10);
+
+      // return true if player wants to leave
+      return true;
+    }
+  }
+  return false;
+}
+
+
+
+
 var fight = function (enemy){
 
     while (playerInfo.health > 0 && enemy.health > 0) {
         
-        var promptFight = window.prompt ("Would you like to FIGHT or SKIP this fight? Enter FIGHT or SKIP to choose.");
-    
-        if (promptFight == "SKIP" || promptFight == "skip") {
-    
-            //confirm player wants to skip
-           var confirmSkip = window.confirm ("Are you sure you'd like to quit?");
-    
-            if (confirmSkip) {
-                window.alert (playerInfo.name + " has decided to skip this fight. Goodbye.");
-    
-                //subtract 2 from player's money
-                playerInfo.money = (0, playerInfo.money - 10);
-
-                console.log("player money" + playerInfo.money);
-
-                break;
-            }
+        if (fightOrSkip()) {
+            break;
         }
     
         // generate random damage value based on player's attack power
@@ -264,5 +289,4 @@ var enemyInfo = [
 ];
 
 
-
-startGame();
+startGame ();
